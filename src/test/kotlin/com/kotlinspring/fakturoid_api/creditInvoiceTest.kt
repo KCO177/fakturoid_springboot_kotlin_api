ccf906b1731b454c3125d56c4630cdd7dec6f4f2
KCO177 <koscelnik.p@gmail.com>
2024-11-19 16:42:25 +0100

    Add logic for credit overflowed remaing items

--- src/test/kotlin/com/kotlinspring/fakturoid_api/creditInvoiceTest.kt
+++ src/test/kotlin/com/kotlinspring/fakturoid_api/creditInvoiceTest.kt
@@ -117,6 +117,65 @@ class creditInvoiceDomainTest {
         assertEquals("SAVER 500 CVS / APPLICATIONS", proformaInvoices.last().lines.first().name)
         assertEquals("final_invoice", proformaInvoices.last().documentType)
     }
+
+    @Test
+    fun `when 100% credits are spent and no validated saver proforma was dealed send offer porforma and switch to buffer system`() { //TODO
+
+        //Given
+        val creditQuantity = 4.0
+        val creditInvoicesMockk = testUtils.createCreditMockkInvoice(quantity = creditQuantity)
+        val subjectsMockk = testUtils.createCreditSubject()
+        val invoiceDataMockk = listOf(testUtils.createClaimData(datesOfCvUploads = List(4) { LocalDate.now() }))
+        val invoicePayload = testUtils.createCreditMockkInvoice(quantity = creditQuantity)
+        val creditInvoiceDomain = CreditInvoiceDomain(creditInvoicesMockk, subjectsMockk, invoiceDataMockk, invoicePayload)
+        //When
+        val proformaInvoices = creditInvoiceDomain.creditInvoices
+
+        //Then
+        assertEquals(1, invoiceDataMockk.size)
+        assert(proformaInvoices.isNotEmpty())
+        assertEquals(2, proformaInvoices.size)
+        assertEquals(
+            "100% of credits applied from total ${creditQuantity.toInt()} credits",
+            proformaInvoices.first().lines.first().name
+        )
+        assertEquals("DO NOT PAY. PAID FROM YOUR CREDITS.", proformaInvoices.first().note)
+        assertEquals(123456, proformaInvoices.first().relatedId)
+        assertEquals("Offer your next Saver 4 CVs / applications should start from the next month", proformaInvoices.last().lines.first().name)
+        assertEquals("proforma", proformaInvoices.last().documentType)
+    }
+
+    @Test
+    fun `when 100% credits are spent and no validated saver dealed switch to buffer system if reached send invoice`() {
+
+        //Given
+        val creditQuantity = 4.0
+        val creditInvoicesMockk = testUtils.createCreditMockkInvoice(quantity = creditQuantity)
+        val subjectsMockk = testUtils.createCreditSubject()
+        val invoiceDataMockk = listOf(testUtils.createClaimData(datesOfCvUploads = List(14) { LocalDate.now() }))
+        val invoicePayload = testUtils.createCreditMockkInvoice(quantity = creditQuantity)
+        val creditInvoiceDomain = CreditInvoiceDomain(creditInvoicesMockk, subjectsMockk, invoiceDataMockk, invoicePayload)
+        //When
+        val proformaInvoices = creditInvoiceDomain.creditInvoices
+
+        //Then
+        assertEquals(1, invoiceDataMockk.size)
+        assert(proformaInvoices.isNotEmpty())
+        assertEquals(2, proformaInvoices.size)
+        assertEquals("100% of credits applied from total ${creditQuantity.toInt()} credits", proformaInvoices.first().lines.first().name)
+        assertEquals("DO NOT PAY. PAID FROM YOUR CREDITS.", proformaInvoices.first().note)
+        println(proformaInvoices.size)
+        println(proformaInvoices.first().lines.first().name)
+        println(proformaInvoices.first().lines.last().quantity)
+        println(proformaInvoices.first().lines.last().name)
+        println(proformaInvoices.last().lines.first().name)
+        assertEquals(123456, proformaInvoices.first().relatedId)
+        assertEquals("Offer your next Saver 4 CVs / applications should start from the next month", proformaInvoices.last().lines.first().name)
+        assertEquals("proforma", proformaInvoices.last().documentType)
+
+    }
+
+
 }
 
 
