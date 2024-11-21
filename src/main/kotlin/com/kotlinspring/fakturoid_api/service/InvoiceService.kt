db97f019f91513a96ce251e16c5c584063f2d8a4
KCO177 <koscelnik.p@gmail.com>
2024-11-21 08:27:09 +0100

    Add logic for credit overflowed remaing items

--- src/main/kotlin/com/kotlinspring/fakturoid_api/service/InvoiceService.kt
+++ src/main/kotlin/com/kotlinspring/fakturoid_api/service/InvoiceService.kt
@@ -18,32 +18,21 @@ class InvoiceService(
     private val finClaimDataRaw: List<ClaimDataDomain> = ClaimDataDomain.getInvoiceData(demoUtils.dbOutput())
     private val finClaim = finClaimDataRaw.filter { it.cvUploadedNumberMonth > 0 }
     private val tenantRegNumbers: List<TenantDomain> = finClaim.map { it.tenant }
-    private val subjects: List<SubjectDomain> = tenantRegNumbers.map { tenant ->
-        subjectService.findOrCreateTenant(
-            bearerToken, SubjectDomain.mapTenantToSubjectDomain(tenant)
-        )
-    }
-
+    private val subjects: List<SubjectDomain> = tenantRegNumbers.map { tenant -> subjectService.findOrCreateTenant(bearerToken, SubjectDomain.mapTenantToSubjectDomain(tenant)) }
     private val invoicesPayload = requireNotNull(invoiceController.getInvoices(bearerToken)) { "Invoices could not be fetched from fakturoid" }
 
     fun createInvoices() {
         val bufferedInvoices: List<InvoiceDomain> = BufferedInvoiceDomain(finClaim, subjects).bufferedInvoice
-        val creditInvoices: List<InvoiceDomain>? = createCreditInvoices()
+        val creditInvoices: List<InvoiceDomain> =
+            invoicesPayload.filter { invoice -> invoice.lines.any { line -> line.name.uppercase().contains("SAVER") } && invoice.documentType != "proforma" }
+            if (creditInvoices.isNotEmpty()) { CreditInvoiceDomain(creditInvoices, subjects, finClaimDataRaw, invoicesPayload).creditInvoices }
+
         val invoices = mutableListOf<InvoiceDomain>().apply {
             if (bufferedInvoices.isNotEmpty()) addAll(bufferedInvoices)
-            creditInvoices?.let { if (it.isNotEmpty()) addAll(it) }
-        }
+            if (creditInvoices.isNotEmpty()) addAll(creditInvoices) }
+
         if (invoices.isNotEmpty()) { invoiceController.createInvoices(bearerToken, invoices) }
     }
 
-    private fun createCreditInvoices(): List<InvoiceDomain>? {
-        val creditInvoices: List<InvoiceDomain> =
-            invoicesPayload.filter { invoice -> invoice.lines.any { line -> line.name.uppercase().contains("SAVER") } && invoice.documentType != "proforma" }
-        if (creditInvoices.isEmpty()) {
-            return null
-        } else {
-            return CreditInvoiceDomain(creditInvoices, subjects, finClaimDataRaw, invoicesPayload).creditInvoices
-        }
-    }
 }
 
